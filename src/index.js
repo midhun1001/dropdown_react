0ed107f3ff4fe663d1593b72eb8351f456068552
midhun1001 <midhun.murali.mm@gmail.com>
2018-04-26 22:11:23 +0530

    fixed bugs. v1.0.1

--- src/index.js
+++ src/index.js
@@ -1,56 +1,171 @@
 import React from 'react';
 import './index.css';
 
-class Fancy extends React.Component {
+class Dropdown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      list: this.props.list,
       input: '',
       showList: false,
-      currentFocus: 0
+      currentFocus: 0,
+      multi: [],
+      arrDir: '',
     };
-    this.compare = (a, b) => {
-      if (a.text < b.text)
-        return -1;
-      if (a.text > b.text)
-        return 1;
-      return 0;
-    };
-    this.toggleList = () => {
-      if (this.state.input.length >= 2) {
-        this.setState({ showList: true });
+    this.FocusEvent = (e, flag) => {
+      e.persist();
+      this.setState({ showList: flag }, () => {
+        if (this.props.multiselect) {
+          const li = document.querySelector('#dp__list ul').childNodes;
+          li[this.state.currentFocus].style.backgroundColor = '#f5f5f5';
+          li[this.state.currentFocus].setAttribute('data-selected', 'active');
+        }
+      });
+      if (flag) {
+        this.setState({ arrDir: 'arrow_down' });
       } else {
-        this.setState({ showList: false });
+        this.setState({ arrDir: '' });
+      }
+      if (this.props.focus) {
+        const focusData = {
+          currentInput: this.state.input,
+          event: e
+        };
+        if (this.props.multiselect) {
+          focusData.multiSelect = this.state.multi;
+        }
+        this.props.focus(focusData);
       }
     };
-    this.changeInput = (e) => {
-      this.setState({ input: e.target.value }, () => {
-        this.toggleList();
-        this.props.callback(this.state.input);
-      });
+    this.changeInput = (e, flag) => {
+      e.persist();
+      const callBack = {};
+      if (flag === 'clear') {
+        this.setState({ input: '', multi: [] });
+        document.getElementById('dp__input').focus();
+      } else {
+        this.setState({ input: e.target.value }, () => {
+          if (this.props.callback) {
+            callBack.currentInput = this.state.input;
+            callBack.event = e;
+            if (this.props.multiselect) {
+              callBack.multiSelect = this.state.multi;
+            }
+            this.props.callback(callBack);
+          }
+        });
+      }
+    };
+    this.defaultWidth = () => {
+      let multiWidth = 0;
+      const parentWidth = document.getElementsByClassName('dp')[0].offsetWidth;
+      const closeWidth = document.getElementsByClassName('dp__clear')[0].offsetWidth;
+      if (this.props.multiselect) {
+        document.getElementsByClassName('dp__selectedInput')[0].offsetWidth;
+      }
+      const defaultWidth = parentWidth - closeWidth;
+      const multiSelectWidth = defaultWidth - multiWidth;
+      return {
+        defaultWidth,
+        multiSelectWidth
+      };
+    };
+    this.customCallback = () => {
+      const callBack = {
+        currentInput: this.state.input,
+        event: e,
+        multiSelect: this.state.multi
+      };
+      this.props.callback(callBack);
+    };
+    this.setInput = (e, input) => {
+      e.persist();
+      if (this.props.multiselect) {
+        if (e.type === 'click') {
+          document.querySelector('#dp__list ul li[data-selected="active"]').style.backgroundColor = '';
+          document.querySelector('#dp__list ul li[data-selected="active"]').removeAttribute('data-selected');
+          e.target.parentNode.setAttribute('data-selected', 'active');
+          e.target.parentNode.style.backgroundColor = "#f5f5f5";
+          const data = e.target.parentNode.getAttribute('data-value');
+          this.setState({ currentFocus: this.state.list.indexOf(data) });
+          document.getElementById('dp__input').focus();
+        }
+        const ele = document.querySelector('#dp__list ul li[data-selected="active"]');
+        const multi = this.state.multi;
+        if (!multi.includes(ele.getAttribute('data-value'))) {
+          multi.push(ele.getAttribute('data-value'));
+          this.setState({ multi, input: '' }, () => {
+            document.getElementById('dp__input').style.width = `${this.defaultWidth().multiSelectWidth}px`;
+            console.log(document.querySelector('#dp__list ul li[data-selected="active"] a span').style.display = 'inline');
+            if (this.props.callback) {
+              this.customCallback();
+            }
+          });
+        }
+      } else {
+        const data = e.target.parentNode.getAttribute('data-value') || document.querySelector('#dp__list ul li[data-selected="active"]').getAttribute('data-value');
+        this.setState({ input: data }, () => {
+          if (this.props.callback) {
+            this.customCallback();
+            document.getElementById('dp__input').focus();
+          }
+        });
+      }
+    };
+    this.removeFromMulti = (val) => {
+      if (this.state.multi.includes(val)) {
+        const multi = this.state.multi;
+        const index = multi.indexOf(val);
+        multi.splice(index, 1);
+        this.setState({ multi }, () => {
+          document.querySelector(`#dp__list ul li[data-value="${val}"]`).style.backgroundColor = '';
+          document.querySelector(`#dp__list ul li[data-value="${val}"]`).removeAttribute('data-selected');
+          document.querySelector(`#dp__list ul li[data-value="${val}"] a span`).style.display = 'none';
+          document.getElementById('dp__input').focus();
+        });
+      }
+    };
+    this.renderMulti = () => {
+      return (
+        this.state.multi.map((val, index) => (
+          <div className="multiselect__item" key={index}>
+            <span>
+              <button onClick={() => this.removeFromMulti(val)}>
+                &#10005;
+              </button>
+              {val}
+            </span>
+          </div>
+        ))
+      );
     };
     this.highlight = (text) => {
-      return { __html: `<strong>${text.substr(0, this.state.input.length)}</strong>${text.substr(this.state.input.length)}` };
+      return { __html: `<strong>${text.substr(0, this.state.input.length)}</strong>${text.substr(this.state.input.length)} <span class="list__selected">Selected</span>` };
     };
     this.renderList = () => {
-      if (this.props.list.length > 0) {
-        const list__arr = this.props.list.sort(this.compare);
+      if (this.state.list.length > 0) {
         const html_li = [];
-        list__arr.map((val, index) => {
+        this.state.list.map((val, index) => {
           if (typeof val === 'object') {
             if (val.text.substr(0, this.state.input.length).toLowerCase() === this.state.input.toLowerCase()) {
               html_li.push(
-                <li data-value={val.text} key={index} style={{ background: `${index === this.state.currentFocus ? '#f5f5f5' : ''}` }}>
+                <li data-value={val.text} key={index}>
                   <a href={val.href} dangerouslySetInnerHTML={this.highlight(val.text)} />
                 </li>
               );
             }
           } else {
-            html_li.push(
-              <li data-value={val} key={index} style={{ background: `${index === this.state.currentFocus ? '#f5f5f5' : ''}` }}>
-                <a dangerouslySetInnerHTML={this.highlight(val)} />
-              </li>
-            );
+            console.log(this.state.input);
+            if (val.substr(0, this.state.input.length).toLowerCase() === this.state.input.toLowerCase()) {
+              html_li.push(
+                <li
+                  data-value={val}
+                  key={index}
+                >
+                  <a dangerouslySetInnerHTML={this.highlight(val)} onClick={(e) => this.setInput(e, val)} />
+                </li>
+              );
+            }
           }
         });
         if (html_li.length === 0) {
@@ -61,32 +176,67 @@ class Fancy extends React.Component {
         return html_li;
       }
     };
-    this.select = (e) => {
-      if (this.state.showList) {
-        let lists = document.querySelector('#dp__list ul').childNodes;
-        if (e.which === 40 && lists[this.state.currentFocus]) {
-          if ((lists.length - 1) !== this.state.currentFocus) {
-            this.setState({ currentFocus: this.state.currentFocus + 1 });
-          }
-        } else if (e.which === 38) {
-          if (this.state.currentFocus > 0) {
-            this.setState({ currentFocus: this.state.currentFocus - 1 });
-          }
-        } else if (e.which === 13) {
-          if (lists[this.state.currentFocus].childNodes[0].getAttribute('href')) {
-            if (this.props.target === 'open') {
-              window.open(lists[this.state.currentFocus].childNodes[0].getAttribute('href'), '_blank');
-            } else {
-              window.location.href = lists[this.state.currentFocus].childNodes[0].getAttribute('href');
-            }
-          }
-          this.setState({ input: lists[this.state.currentFocus].getAttribute('data-value') });
+    this.scrollList = (dir, ele) => {
+      if (dir === 'down') {
+        const divHeight = document.getElementById('dp__list').offsetHeight;
+        const scrollPos = document.getElementById('dp__list').scrollTop;
+        const elePos = ele.offsetTop;
+        const eleHei = ele.offsetHeight;
+        if (elePos > divHeight) {
+          document.getElementById('dp__list').scrollTop = scrollPos + eleHei;
         }
-        return false;
+      } else {
+        const scrollPos = document.getElementById('dp__list').scrollTop;
+        const elePos = ele.offsetTop;
+        const eleHei = ele.offsetHeight;
+        if (elePos < scrollPos) {
+          document.getElementById('dp__list').scrollTop = scrollPos - eleHei;
+        }
       }
     };
-
-    // Styles
+    this.select = (e) => {
+      const li = document.querySelector('#dp__list ul').childNodes;
+      if (e.which === 40) {
+        if (li.length - 1 > this.state.currentFocus) {
+          this.setState({ currentFocus: this.state.currentFocus + 1 }, () => {
+            const prev = li[this.state.currentFocus - 1];
+            const next = li[this.state.currentFocus];
+            if (prev) {
+              prev.style.backgroundColor = '';
+              prev.removeAttribute('data-selected');
+            }
+            if (next) {
+              next.style.backgroundColor = '#f5f5f5';
+              next.setAttribute('data-selected', 'active');
+              this.scrollList('down', next);
+            }
+          });
+        }
+      } else if (e.which === 38) {
+        if (this.state.currentFocus !== 0) {
+          const prev = li[this.state.currentFocus - 1];
+          const next = li[this.state.currentFocus];
+          prev.style.backgroundColor = '#f5f5f5';
+          prev.setAttribute('data-selected', 'active');
+          next.style.backgroundColor = '';
+          next.removeAttribute('data-selected');
+          this.scrollList('up', prev);
+          this.setState({ currentFocus: this.state.currentFocus - 1 });
+        }
+      } else if (e.which === 13) {
+        this.setInput(e);
+      }
+      if (this.props.keydown) {
+        const focusData = {
+          currentInput: this.state.input,
+          event: e
+        };
+        if (this.props.multiselect) {
+          focusData.multiSelect = this.state.multi;
+        }
+        this.props.keydown(focusData);
+      }
+    };
     this.placeholder = 'Enter your text';
     if (props) {
       if (props.placeholder) {
@@ -94,32 +244,53 @@ class Fancy extends React.Component {
       }
     }
   }
+  componentDidMount() {
+    document.getElementById('dp__input').style.width = `${this.defaultWidth().defaultWidth}px`;
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.list !== this.props.list) {
+      this.setState({ list: nextProps.list });
+    }
+  }
   render() {
     return (
-      <div className={`dp ${this.props.containerClass ? this.props.containerClass: ''}`}>
-        <input
-          id="dp__input"
-          className={`dp__input ${this.props.inputClass ? this.props.inputClass : ''}`}
-          value={this.state.input}
-          onChange={(e) => this.changeInput(e)}
-          placeholder={this.placeholder}
-          onKeyDown={(e) => this.select(e)}
-          // onBlur={() => this.setState({ showList: false })}
-        />
-        {
-          this.state.showList &&
-          <div
-            id="dp__list"
-            className={`dp__list ${this.props.listClass ? this.props.listClass : ''}`}
-            style={this.dp__list_style}
-            >
-            <ul>
-              { this.renderList() }
-            </ul>
+      <div
+            className={`dp ${this.props.containerClass ? this.props.containerClass: ''}` }
+          >
+            {
+              this.props.multiselect && this.props.multiselect === true &&
+              <div className="dp__selectedInput">
+                {this.renderMulti()}
+              </div>
+            }
+            <input
+              id="dp__input"
+              className={`dp__input ${this.props.inputClass ? this.props.inputClass : ''}`}
+              value={this.state.input}
+              placeholder={this.placeholder}
+              onChange={(e) => this.changeInput(e)}
+              onKeyDown={(e) => this.select(e)}
+              onFocus={(e) => this.FocusEvent(e, true)}
+              onBlur={() => this.setState({ showList: false })}
+            />
+            <button className="dp__clear" onClick={(e) => this.changeInput(e, 'clear')}>
+              <span className={`${this.state.arrDir} arrow`} >&#8227;&nbsp;</span>
+              <span>&#10005;</span>
+             </button>
+            {
+              this.state.showList &&
+              <div
+                id="dp__list"
+                className={`dp__list ${this.state.listClass ? this.state.listClass : ''}`}
+                style={this.dp__list_style}
+                >
+                <ul>
+                  { this.renderList() }
+                </ul>
+              </div>
+            }
           </div>
-        }
-      </div>
     );
   }
 }
-export default Fancy;
+export default Dropdown;
